7f26cc8cdfe98eaab913912e35c5d94d83fdd7ec
hugosilvaguerreiro <hugo.guerreiro@tecnico.ulisboa.pt>
2017-10-26 20:17:49 +0100

    install file added

---
+++ install.sh
@@ -0,0 +1,5 @@
+sudo -H pip install --upgrade pip setuptools wheel
+sudo -H apt-get install python-pyaudio python3-pyaudio
+sudo -H pip install --upgrade pocketsphinx
+sudo -H pip install pyttsx
+sudo -H pip install SpeechRecognition
