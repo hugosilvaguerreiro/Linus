7ec404f4faccfce4943d8a4829d2c556141429b3
hugosilvaguerreiro <hugo.guerreiro@tecnico.ulisboa.pt>
2017-10-27 15:07:43 +0100

    install update 2

--- install.sh
+++ install.sh
@@ -6,7 +6,10 @@ sudo apt-get install swig
 sudo apt install python-pip
 sudo -H pip install --upgrade pip setuptools wheel
 sudo -H apt-get install python-pyaudio python3-pyaudio
-sudo pip install pyaudio --upgrade
+sudo -H pip install pyaudio --upgrade
 sudo -H pip install --upgrade pocketsphinx
 sudo -H pip install pyttsx
 sudo -H pip install SpeechRecognition
+sudo apt-get install gspread
+sudo -H pip install gspread
+sudo -H pip install --upgrade oauth2client
